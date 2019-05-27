48eb3bee64511ccc1b73267639f8fa335707784b
PCV <PCV@DESKTOP-8V2VJ0C.local>
2019-05-27 16:30:04 -0500

    Add PetDatabase back on to the project. Somehow it was dropped.

---
+++ src/PetDatabase/PetDatabase.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PetDatabase;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author PCV
+ */
+
+public class PetDatabase {
+
+    private ArrayList<Pet> pets = new ArrayList<Pet>();
+
+    public void add(Pet p) {
+        pets.add(p);
+    }
+
+    public ArrayList<Pet> getPets() {
+        return pets;
+    }
+
+    public int getCount() {
+        return pets.size();
+    }
+
+    public void remove(int id) {
+        pets.remove(id);
+    }
+
+}
